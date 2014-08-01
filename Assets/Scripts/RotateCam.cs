1729eb483a6d005a02c541954779ceba783fbcd8
Francesco Maoli <fran922003@yahoo.com>
2014-08-01 14:50:57 -0400

    *Added rotate camera script

---
+++ Assets/Scripts/RotateCam.cs
@@ -0,0 +1,55 @@
+﻿// Francesco Maoli
+// 8/1/2014
+// 8/1/2014
+// Attach to "Main Camera" object
+// Currently needs work. Directional inputs do not correspond to new camera position (eg. up becomes left).
+
+using UnityEngine;
+using System.Collections;
+
+public class RotateCam : MonoBehaviour {
+
+	public GameObject targetObject;
+	private float targetAngle = 0;
+	const float rotationAmount = 1.5f;
+	public float rDistance = 1.0f;
+	public float rSpeed = 1.0f;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+		// Trigger functions if Rotate is requested
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			targetAngle -= 90.0f;
+		} else if (Input.GetKeyDown(KeyCode.E)) {
+			targetAngle += 90.0f;
+		}
+		
+		if(targetAngle !=0)
+		{
+			Rotate();
+		}
+	}
+	
+	protected void Rotate()
+	{
+		
+		float step = rSpeed * Time.deltaTime;
+		float orbitCircumfrance = 2F * rDistance * Mathf.PI;
+		float distanceDegrees = (rSpeed / orbitCircumfrance) * 360;
+		float distanceRadians = (rSpeed / orbitCircumfrance) * 2 * Mathf.PI;
+		
+		if (targetAngle>0)
+		{
+			transform.RotateAround(targetObject.transform.position, Vector3.up, -rotationAmount);
+			targetAngle -= rotationAmount;
+		}
+		else if(targetAngle <0)
+		{
+			transform.RotateAround(targetObject.transform.position, Vector3.up, rotationAmount);
+			targetAngle += rotationAmount;
+		}
+		
+	}
+}

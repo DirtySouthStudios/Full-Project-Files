d2c00b3b3e842559f73380f8752341c3fb3db9e5
FrankyG170 <fran922003@yahoo.com>
2014-08-11 19:21:30 -0400

    *Created accurate Ray representation of mouse, needs precision from angle offset

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,70 +7,65 @@ using UnityEngine;
 using System.Collections;
 
 public class Weapon : MonoBehaviour {
-	
+
+	public float fireRate = 0;
 	public LayerMask notToHit;
 	public Transform firePointPosition;
 
 	float timeToFire = 0;
+
+	private Camera ref_mainCamera;
+	
+	public Vector3 mousePosition;
+	private Plane zeroYPlane;
 
 	void Awake () {
 		/*firePointPosition = transform.FindChild ("FirePoint");
 		if (firePointPosition == null) {
 			Debug.LogError ("No firePoint? WHAT?!");		
 		}*/
+
+		ref_mainCamera = GameObject.FindGameObjectWithTag("MainCamera").camera;
+		//This is the tool you need - plane
+		zeroYPlane = new Plane(Vector3.up, Vector3.zero);
 	}
 
 	void Update () {
-		
-		RaycastHit[] hits;
-		Vector3 ray_point_onfloor;
-		hits = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward);
-		int i = 0;
-		while (i < hits.Length) {
-			RaycastHit hit = hits[i];
-			
-			if (hit.transform.tag == "floor") {
-				ray_point_onfloor = hit.point;
-				Debug.DrawLine (transform.position, ray_point_onfloor);
-				break;
 
-		/*
-		Vector3 mousePostion = new Vector3 ((Camera.main.ScreenToWorldPoint (Input.mousePosition).x), 0, (Camera.main.ScreenToWorldPoint (Input.mousePosition).z));
-		Vector3 firePointPosition = new Vector3 (firePoint.position.x, firePoint.position.y, firePoint.position.z);
-		Debug.DrawLine (firePointPosition, mousePostion);
-
-		Vector3 mousePostion = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		mousePostion.y = 0;
-		Vector3 firePointPosition = new Vector3 (firePoint.position.x, firePoint.position.y, firePoint.position.z);
+		// Fire ray from Camera to mouse
+		Ray ray = ref_mainCamera.ScreenPointToRay(Input.mousePosition);
 		
-		float distToMouse = Vector3.Distance(mousePostion, firePointPosition) ;
 		
-		RaycastHit hit;
-		Ray myRay = new Ray (firePointPosition, mousePostion);
-		
-        ////////////////////////////////////////////////////
-	    RaycastHit[] hits;
-        Vector3 ray_point_onfloor;
-        hits = Physics.RaycastAll(firePointPosition mousePosition);
-        int i = 0;
-        while (i < hits.Length) {
-            RaycastHit hit = hits[i];
+		float _hitDistance;
+		//cast ray using zerpYplane to find out mouse position the ray intersects it
+		zeroYPlane.Raycast(ray, out _hitDistance);
+		mousePosition = ray.GetPoint(_hitDistance);
 
-            if (hit.tag == "floor") {
-                ray_point_onfloor= hit.point;
-				break;
-            }
-            i++;
-        }
+		mousePosition = new Vector3 (mousePosition.x, mousePosition.y = 1, mousePosition.z);
+		Debug.DrawLine (transform.position, mousePosition);
 
-		Debug.DrawRay (firePointPosition, mousePostion);
+		/* 
+		 * Could be useful in future KEEP
+		 * RaycastHit[] hits;
+		 * Vector3 ray_point_onfloor;
+		 * hits = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward);
+		 * int i = 0;
+		 * while (i < hits.Length) {
+		 * RaycastHit hit = hits[i];
+		 * 		if (hit.transform.tag == "floor") {
+		 * 			ray_point_onfloor = hit.point;
+		 * 			Debug.DrawLine (transform.position, ray_point_onfloor);
+		 * 			break;
+		 * 		}
+		 * 		i++;
+		 * }
+		 */
 
 		if (fireRate == 0) {
-			if (Input.GetButtonDown("Fire1") && Physics.Raycast(myRay, out hit, distToMouse, notToHit)) {
+			if (Input.GetButtonDown("Fire1")) {
 				Shoot();
 
 			}
-			i++;
 		}
 	}
 
